0eecd34dbb44b4c849baec1790819f98d552c1d6
Tatyana404 <kot.kate2020@gmail.com>
2021-09-24 23:20:08 +0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+;(function (activeSlide) {
+  const slides = document.querySelectorAll('.slide')
+
+  slides[activeSlide].classList.add('active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
+  }
+
+  function clearActiveClasses () {
+    slides.forEach(slide => {
+      slide.classList.remove('active')
+    })
+  }
+})(0)
